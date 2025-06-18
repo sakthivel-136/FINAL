5f3feebb03a49a2acfc4b4266cbf1c79c95aebd3
SAKTHIVEL <c.sakthivel1.3.2006@gmail.com>
2025-06-18 07:08:25 +0530

    Update mic_chat.py

--- mic_chat.py
+++ mic_chat.py
@@ -9,9 +9,7 @@ Original file is located at
 
 
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer, WebRtcMode
-# Import AudioProcessorBase and ClientSettings from sub_modules
-from streamlit_webrtc.sub_modules import AudioProcessorBase 
+from streamlit_webrtc import webrtc_streamer, WebRtcMode, ClientSettings, AudioProcessorBase
 import numpy as np
 import threading
 import queue
