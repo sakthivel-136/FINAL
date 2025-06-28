40bf1a7aa3ca4cec70791984f68ca673e1f41124
SAKTHIVEL <c.sakthivel1.3.2006@gmail.com>
2025-06-29 01:36:19 +0530

    Update final_py.py

--- final_py.py
+++ final_py.py
@@ -149,7 +149,6 @@ if st.session_state.page == 3:
     st.title("💬 Chat with KCET Bot - Page 3")
 
    theme_index = st.slider("🎨 Slide to Choose Theme", min_value=0, max_value=2, value=0, format="%d")
-
 themes = [
     ("Blue & Purple", "#d0e8f2", "#d1d1e9"),
     ("Green & Gray", "#c6f6d5", "#e2e8f0"),
